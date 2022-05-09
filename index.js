0663759e392c6e3d6b45bf857d29e7a3eb22dd49
ZacLima91 <jadielsonmanoel@gmail.com>
2022-05-09 20:55:57 -0300

    configurei o arquivo filme.js para o deploy no render

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-import express from 'express'
-import { routers } from './src/routes/routers.js'
-import path from 'path'
-import dotenv from 'dotenv'
+import express from "express";
+import { routers } from "./src/routes/routers.js";
+import path from "path";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const port = 3009
-let __dirname = path.resolve(path.dirname(''))
+const app = express();
+const port = process.env.PORT || 3009;
+let __dirname = path.resolve(path.dirname(""));
 app.use(express.json());
 app.use(express.urlencoded());
 
-console.log(__dirname)
+console.log(__dirname);
 
-app.set('view engine', 'ejs')
-app.use(routers)
-app.use(express.static(path.join(__dirname, 'public')))
-
+app.set("view engine", "ejs");
+app.use(routers);
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
-    console.log(`Estou rodando na porta ${port}`)
-})
+  console.log(`Estou rodando na porta ${port}`);
+});
