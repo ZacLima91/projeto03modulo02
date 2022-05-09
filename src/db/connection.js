d5d4d14c1bb41a4c6e5aa24a01d75eae980341f2
ZacLima91 <jadielsonmanoel@gmail.com>
2022-05-09 20:16:09 -0300

    configurei o arquivo filme.js para o deploy no render

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,12 +3,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const connection = new Sequelize(process.env.DB_URL, {
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+export const connection = new Sequelize(process.env.DB_URL,
+//     {
+//   dialect: "postgres",
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,
+//     },
+//   },
+// }
+);
